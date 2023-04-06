8ed343bd46a572da58810a02d8851454bc30ee45
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-04-06 14:04:09 +0200

    mise en place de la demo

---
+++ Assets/Scripts/Object Script/PostItScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostItScript : MonoBehaviour
+{
+    [SerializeField]
+    ChefDorchestre chef;
+
+    [SerializeField]
+    DialogSystem dialogSystem;
+
+    [SerializeField]
+    TextAsset dialog;
+    
+    public void Interact()
+    {
+        //new System.Action[] { coucou, coucou2 }
+        chef.SwitchSystem(ChefDorchestre.GameSystem.Dialogue);
+        dialogSystem.StartDialogue(dialog, null);
+    }
+
+}
