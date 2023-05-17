430870f0d97231902c30ad68eb861ee4e8b091f6
Chloé <qhermier@gmail.com>
2023-05-17 13:42:11 +0200

    souris fixed

--- Assets/Scripts/Souris.cs
+++ Assets/Scripts/Souris.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Souris : MonoBehaviour
 {
 
-    public float speed = 0.1f;
+    public float speed = -0.01f;
     public bool isMoving = false;
+
+
 
     // Update is called once per frame
     void Update()
     {
         if (isMoving)
         {
+            if (transform.position.x >= 10) {
+                speed = -speed;
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            } else if (transform.position.x <= -10)
+            {
+                speed = -speed;
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            }
+
             transform.position = new Vector3(transform.position.x + speed, transform.position.y);
         }
-    }
+        }
 
     void Interact()
     {
